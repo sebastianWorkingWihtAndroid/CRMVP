785ebf5de4860273a8202ce94f15b36b371d6ccc
Sebastian Miguel <109224802+sebastianWorkingWihtAndroid@users.noreply.github.com>
2023-06-30 16:53:28 -0500

    se creo el moddelo

---
+++ app/src/main/java/com/embriotecno/entradasysalidas/Modelo/ModelUser.java
@@ -0,0 +1,12 @@
+package com.embriotecno.entradasysalidas.Modelo;
+
+public class ModelUser {
+
+    private String nombres;
+    private String user;
+    private String clave;
+
+    public ModelUser(){}
+
+
+}
